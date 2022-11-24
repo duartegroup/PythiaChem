8ac4035f6d8d05a1c117ad4a4f6943dae2ac61fd
Bernie Lee <bernadette.lee.cy@gmail.com>
2022-11-24 15:41:32 +0000

    test (comment out sns.set)

--- Modules/plotting_sklearn.py
+++ Modules/plotting_sklearn.py
@@ -15,7 +15,7 @@ from sklearn.metrics import PrecisionRecallDisplay
 from sklearn.metrics import average_precision_score
 
 # imbalanced
-from imblearn.metrics import classification_report_imbalanced 
+from imblearn.metrics import classification_report_imbalanced
 from imblearn.metrics import sensitivity_specificity_support
 
 # matplotlib
@@ -28,48 +28,48 @@ import seaborn as sns
 from . import classification_metrics as cmet
 
 # logging
-import logging 
+import logging
 
 
 def roc_curve_data(ytest, probs, pos_label=1):
     """
     """
-    
+
     log = logging.getLogger(__name__)
     fp, tp, thresholds = roc_curve(ytest, probs, pos_label=pos_label)
     roc_auc = cmet.auc(fp, tp)
     log.debug("ROC analysis class: {}\n\tTrue positives:\n\t{}\n\tFalse positives:\n\t{}\n\tAUC: {}".format(pos_label, tp, fp, roc_auc))
-    
+
     return fp, tp, thresholds, roc_auc
-        
-def plot_roc_curve(probas, y_test, title="ROC Curve", x_lab="False Positive Rate (FPR)", 
+
+def plot_roc_curve(probas, y_test, title="ROC Curve", x_lab="False Positive Rate (FPR)",
                    y_lab="True Positive Rate (TPR)", axes=None, figsize=None, plot_class="all",
                   col_map="viridis", savefigure=False, filename=None, size=(10,10), fontsize=15,
                   return_raw_data=False):
     """
     """
-    
+
     log = logging.getLogger(__name__)
-                   
+
     if not isinstance(probas, np.ndarray):
         probas = np.array(probas)
-    
+
     if not isinstance(y_test, np.ndarray):
         y_test = np.ndarray(y_test)
-        
+
     if isinstance(plot_class, str) or  isinstance(plot_class, None):
         plot_class = list(set(y_test))
         data = {int(k):{} for k in plot_class}
     else:
         data = {int(k):{} for k in plot_class}
-        
+
     if axes is None:
         fig, axes = plt.subplots(1, 1, figsize=size)
 
     for cls in plot_class:
         cls = int(cls)
         #log.info("probas {} {}".format(probas, probas.shape))
-        data[cls]["fpr"], data[cls]["tpr"], data[cls]["thresholds"], data[cls]["auc"] = roc_curve_data(y_test, probas[:, cls], 
+        data[cls]["fpr"], data[cls]["tpr"], data[cls]["thresholds"], data[cls]["auc"] = roc_curve_data(y_test, probas[:, cls],
                                                                                                        pos_label=cls)
         col = plt.cm.get_cmap(col_map)(float(cls)/len(plot_class))
         axes.plot(data[cls]["fpr"],  data[cls]["tpr"], color=col, label="Class {} (auc={:.2f})".format(cls, data[cls]["auc"]))
@@ -83,14 +83,14 @@ def plot_roc_curve(probas, y_test, title="ROC Curve", x_lab="False Positive Rate
         axes.tick_params(labelsize=fontsize)
         axes.grid(True)
         #plt.show()
-        
+
         if savefigure is True and ax is None:
             if filename is not None:
                 plt.savefig("{}".format(filename))
             else:
                 filename = "roc.png"
                 plt.savefig("roc.png")
-    
+
     if not return_raw_data:
         return axes
     else:
@@ -103,38 +103,38 @@ def precision_recall_data(y_test, probs, pos_label=1):
     prec, rec, thresholds = precision_recall_curve(y_test, probs, pos_label=pos_label)
     average_precision = average_precision_score(y_test, probs)
     log.debug("precision recall analysis class: {}\n\tPrecision:\n\t{}\n\tRecall:\n\t{}\n\tAverage precision: {}".format(pos_label, prec, rec, average_precision))
-    
+
     return prec, rec, thresholds, average_precision
-    
 
 
-def plot_pr_curve(probas, y_test, title="Precision Recall Curve", x_lab="Recall", 
+
+def plot_pr_curve(probas, y_test, title="Precision Recall Curve", x_lab="Recall",
                    y_lab="Precision", axes=None, figsize=None, plot_class="all",
                   col_map="viridis", savefigure=False, filename=None, size=(10,10), fontsize=15,
                   return_raw_data=False):
     """
     """
-    
+
     log = logging.getLogger(__name__)
-                   
+
     if not isinstance(probas, np.ndarray):
         probas = np.array(probas)
-    
+
     if not isinstance(y_test, np.ndarray):
         y_test = np.ndarray(y_test)
-        
+
     if isinstance(plot_class, str) or  isinstance(plot_class, None):
         plot_class = list(set(y_test))
         data = {k:{} for k in plot_class}
     else:
         data = {k:{} for k in plot_class}
-        
+
     if axes is None:
         fig, axes = plt.subplots(1, 1, figsize=size)
 
     for cls in plot_class:
         cls = int(cls)
-        data[cls]["precision"], data[cls]["recall"], data[cls]["thresholds"], data[cls]["ap"] = precision_recall_data(y_test, probas[:, cls], 
+        data[cls]["precision"], data[cls]["recall"], data[cls]["thresholds"], data[cls]["ap"] = precision_recall_data(y_test, probas[:, cls],
                                                                                                        pos_label=cls)
         col = plt.cm.get_cmap(col_map)(float(cls)/len(plot_class))
         axes.plot(data[cls]["recall"],  data[cls]["precision"], color=col, label="Class {} (Average Precision={:.2f})".format(cls, data[cls]["ap"]))
@@ -148,34 +148,34 @@ def plot_pr_curve(probas, y_test, title="Precision Recall Curve", x_lab="Recall"
         axes.tick_params(labelsize=fontsize)
         axes.grid(True)
         #plt.show()
-        
+
         if savefigure is True and ax is None:
             if filename is not None:
                 plt.savefig("{}".format(filename))
             else:
                 filename = "pr.png"
                 plt.savefig("pr.png")
-    
+
     if not return_raw_data:
         return axes
     else:
         return axes, data
-            
+
 def plot_confusion_matrix(cmx, axes=None, col_map="RdYlBu", labels=(0, 1), title="Confusion Matrix", x_label="Predicted Class", y_label="Known Class", fontsize=20,
                           annotate = False, vmin=None, vmax=None):
     """
     """
     log = logging.getLogger(__name__)
-    
+
     log.info("{}\n{}".format(cmx, labels))
-    
+
     cmx_df = pd.DataFrame(data=cmx, columns=labels, index=labels)
-    sns.set(font_scale=1.4)
+    #sns.set(font_scale=1.4)
     axes = sns.heatmap(cmx_df, annot=True, ax=axes, fmt="d")
     axes.set_title(title, fontsize=fontsize+2)
     axes.set_xlabel(x_label, fontsize=fontsize+1)
     axes.set_ylabel(y_label, fontsize=fontsize+1)
-    
+
     display(axes)
     return axes
 
