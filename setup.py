78e537dc1d007281f0061bf8f1d4090a11225eb6
Bernie Lee <bernadette.lee.cy@gmail.com>
2022-12-02 12:03:13 +0000

    Reorganise files and make package pip installable

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Pythia",
+    version="0.1",
+    description="ML workflow functions",
+    author="Matina, Ally, Bernie",
+    packages=["Pythia"],
+)
